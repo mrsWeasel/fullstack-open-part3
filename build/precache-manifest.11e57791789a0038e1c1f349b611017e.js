d1d5f3b9788e6beb0063b8b57d2ffe82b535b6e0
Heino Laura <laura.2.heino@sok.fi>
2022-04-09 22:41:41 +0300

    3.11 tuotantoversio

---
+++ build/precache-manifest.11e57791789a0038e1c1f349b611017e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341504ca6d08a471a80812d2f67cd9be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dff873f81bd05ec0d209",
+    "url": "/static/js/2.0ec53de7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0ec53de7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "749c27975ddcfdacf15e",
+    "url": "/static/js/main.0664297a.chunk.js"
+  },
+  {
+    "revision": "57d8af95778078760a0e",
+    "url": "/static/js/runtime-main.283795fd.js"
+  }
+]);
